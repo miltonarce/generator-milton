52c2e95755a0290f898cf516ab6b7a5735718043
Milton Arce <miltonhernan.arce@gmail.com>
2022-10-11 14:09:31 -0300

    Fix on add dependency

--- generators/react/app/simple/index.js
+++ generators/react/app/simple/index.js
@@ -2,7 +2,7 @@ const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   installDependencies() {
-    this.npmInstall(["react-router-dom"]);
+    Generator.addDependencies({dependency: "react-router-dom"});
   }
 
   app() {
