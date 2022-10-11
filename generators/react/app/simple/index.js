4679d95c29890a000744a660a467a61f54d74f15
Milton Arce <miltonhernan.arce@gmail.com>
2022-10-11 13:08:04 -0300

    Add react simple configuration

---
+++ generators/react/app/simple/index.js
@@ -0,0 +1,11 @@
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  installDependencies() {
+    this.npmInstall(["react-router-dom"]);
+  }
+
+  app() {
+    this.fs.copy(this.templatePath("app"), this.destinationPath("./src/app"));
+  }
+};
--- generators/react/index.js
+++ generators/react/index.js
@@ -1,7 +1,68 @@
 const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
-  method1() {
-    this.log("method 1 just ran");
+  async prompting() {
+    const answers = await this.prompt([
+      {
+        type: "list",
+        name: "app",
+        message: "What kind of app do you want?",
+        choices: [
+          {
+            name: "Simple React App (Vite)",
+          },
+          {
+            name: "NextJs App",
+          },
+        ],
+      },
+      {
+        type: "checkbox",
+        name: "features",
+        message: "What features do you want to add",
+        when: (answers) => answers.app === "Simple React App (Vite)",
+        choices: [
+          {
+            name: "eslint",
+            checked: true,
+          },
+          {
+            name: "typescript",
+          },
+          {
+            name: "router",
+          },
+          {
+            name: "husky",
+          },
+        ],
+      },
+      {
+        type: "checkbox",
+        name: "features",
+        message: "What features do you want to add",
+        when: (answers) => answers.app === "NextJs App",
+        choices: [
+          {
+            name: "eslint",
+            checked: true,
+          },
+          {
+            name: "typescript",
+          },
+          {
+            name: "husky",
+          },
+        ],
+      },
+    ]);
+
+    if (answers.app) {
+      if (answers.app === "Simple React App (Vite)") {
+        this.composeWith(require.resolve("./app/simple"));
+      } else if (answers.app === "NextJs App") {
+        this.composeWith(require.resolve("./app/nextjs"));
+      }
+    }
   }
 };
